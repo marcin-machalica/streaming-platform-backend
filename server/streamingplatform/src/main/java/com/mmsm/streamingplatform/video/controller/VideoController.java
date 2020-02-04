a7a11354d861dd15aca0dc03b4be9f6007c1af7c
marszm <mariusz.szmer@gmail.com>
2020-02-04 19:00:44 +0100

    can open video path to video in browser

--- server/streamingplatform/src/main/java/com/mmsm/streamingplatform/video/controller/VideoController.java
+++ server/streamingplatform/src/main/java/com/mmsm/streamingplatform/video/controller/VideoController.java
@@ -6,15 +6,23 @@ import com.mmsm.streamingplatform.video.service.VideoRepository;
 import com.mmsm.streamingplatform.video.service.VideoService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,16 +41,16 @@ public class VideoController {
     @GetMapping("/download/{id}")
     public ResponseEntity<InputStreamResource> getMeasurement(@PathVariable("id") Long id) throws FileNotFoundException {
         Optional<Video> video = videoRepository.findById(id);
-        if (!video.isPresent()) {
+        if (video.isEmpty()) {
             return null;
         }
-        
+
         File file = new File(video.get().getPath());
         InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
         return ResponseEntity.ok()
                 .contentLength(file.length())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .contentType(MediaType.parseMediaType("video/mp4"))
                 .body(resource);
     }
 }
